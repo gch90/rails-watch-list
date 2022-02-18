7e2b6125121743b380a1543c1f19bc04a9f5ac1d
Georges Chami <g.chami90@gmail.com>
2022-02-18 16:51:02 -0500

    completed the functional part of the app

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,12 +1,29 @@
 class BookmarksController < ApplicationController
   def new
-
     @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
   end
 
   def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
+
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
   end
 
-  def delete
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    @list = @bookmark.list_id
+    redirect_to list_path(@list)
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'bookmarks/new'
-  get 'bookmarks/create'
-  get 'bookmarks/delete'
+  # get 'bookmarks/new'
+  # get 'bookmarks/create'
+  # get 'bookmarks/delete'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :lists, only: [:index, :show, :new, :create] do
-    resources :bookmarks, only: [:new, :create, :delete]
+    resources :bookmarks, only: [:new, :create]
   end
+  resources :bookmarks, only: [:destroy]
 end
